050750b46a772523d74609b608040f9f59e74eab
Leandro Ribeiro <leandro-hdg@live.com>
2022-04-09 18:32:13 -0300

    listando usuários

---
+++ src/data/users.js
@@ -0,0 +1,33 @@
+export default [
+    {
+        id: 1,
+        name: 'Leandro Ribeiro',
+        email: 'leribeiro@email.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/12/13/16/17/dancer-1904467_960_720.png'
+    },
+    {
+        id: 2,
+        name: 'Julia Silva',
+        email: 'jujusilva@email.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2022/03/27/23/57/woman-7096290_960_720.png'
+    },
+    {
+        id: 3,
+        name: 'Rafael Gomes',
+        email: 'rafarafa@email.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_960_720.png'
+    },
+    {
+        id: 4,
+        name: 'Carol Monteiro',
+        email: 'carolmm@email.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/08/20/05/36/avatar-1606914_960_720.png'
+    },
+    {
+        id: 5,
+        name: 'Roy Assis',
+        email: 'royassis@email.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2012/04/01/18/02/golf-23794_960_720.png'
+    },
+
+]
