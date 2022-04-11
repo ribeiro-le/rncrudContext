3042dcb88bb806d8e319d7efa2c5b97b60f35de3
Leandro Ribeiro <leandro-hdg@live.com>
2022-04-11 03:07:19 -0300

    projeto finalizado

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,7 @@ module.exports = {
   root: true,
   extends: '@react-native-community',
 
-
   trailingComma: 'all',
 };
 
-""
+
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+
   arrowParens: 'avoid',
   bracketSameLine: true,
   bracketSpacing: false,
